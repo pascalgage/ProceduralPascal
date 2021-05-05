4fd2f264e87fb1457059c4f30bda3eaf847e289e
Pascal GAGE <PGage@arfp.asso.fr>
2021-05-05 10:04:09 +0200

    changement projet

---
+++ InversionAetB/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversionAetB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+
+            double temp;
+
+            //saisie des entrées
+            Console.WriteLine("Entrez une valeur pour a :");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entrez une valeur pour b :");
+            b = double.Parse(Console.ReadLine());
+
+            //traitement
+            temp = a;
+            a = b;
+            b = temp;
+
+            //Affichage du résultat
+            Console.WriteLine("a est égal à:" + a);
+            Console.WriteLine("b est égal à:" + b);
+
+
+
+
+            Console.ReadKey();
+        }
+        public static void INVERSION(ref double a, ref double b)
+        {
+
+        }
+    }
+}
